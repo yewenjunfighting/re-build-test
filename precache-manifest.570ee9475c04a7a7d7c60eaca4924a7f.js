101360ee31c0eaa552e04d99f94746e9e98c1a4c
wenjun ye <1728914873@qq.com>
2019-04-09 20:36:05 +0800

    Updates

---
+++ precache-manifest.570ee9475c04a7a7d7c60eaca4924a7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "417756dbe7897ca99dff",
+    "url": "./static/js/main.63c6fd60.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "./static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "417756dbe7897ca99dff",
+    "url": "./static/css/main.1243fe51.chunk.css"
+  },
+  {
+    "revision": "2db94e6d8b5e5efe6b41cbd824884d31",
+    "url": "./index.html"
+  }
+];
