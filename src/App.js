e8272d1de663c69d52a0d9c8176950a649f7ec82
wenjun ye <1728914873@qq.com>
2019-04-10 17:24:00 +0800

    Mobile adaptation

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-           叶文俊,是个狼人
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                hello vw-layout
+            </div>
+        );
+    }
 }
 
 export default App;
